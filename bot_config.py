3e44af452e0299288b813d638f75a9ff5f61b971
Jen Thakar <jathak@gmail.com>
2017-08-23 21:39:11 -0700

    Prep for dokku

--- bot_config.py
+++ bot_config.py
@@ -1,4 +1,11 @@
-token = "SLACK-BOT-TOKEN"
-piazza_id = "PIAZZA-COURSE-ID"
-piazza_email = "PIAZZA-ACCOUNT-EMAIL"
-piazza_password = "PIAZZA-ACCOUNT-PASSWORD"
+default_token = "SLACK-BOT-TOKEN"
+default_piazza_id = "PIAZZA-COURSE-ID"
+default_piazza_email = "PIAZZA-ACCOUNT-EMAIL"
+default_piazza_password = "PIAZZA-ACCOUNT-PASSWORD"
+
+import os
+
+token = os.environ.get('PIAZZA_SLACK_BOT_TOKEN', default_token)
+piazza_id = os.environ.get('PIAZZA_SLACK_BOT_COURSE', default_piazza_id)
+piazza_email = os.environ.get('PIAZZA_SLACK_BOT_EMAIL', default_piazza_email)
+piazza_password = os.environ.get('PIAZZA_SLACK_BOT_PASSWORD'), default_piazza_password)
